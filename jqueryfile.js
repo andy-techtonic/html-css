cb24d59f680faaeabaee358c4caf470e29ead9d7
Andy Knolla <andy@techtonicgroup.com>
2017-01-20 23:24:22 -0700

    Clean some stuff up to make explaining this code easier.

--- jqueryfile.js
+++ jqueryfile.js
@@ -1,13 +1,12 @@
 
-// this line ensures that the dom has rendered before this function runs
 $(document).ready(function() {
-
       // make template
       var template = $('#blog-template').clone();
+                                                          console.log(template.html());
 
       // attach event handler to submit button
-      $('#new-blog-input').on('submit', function(event) {       // event provided for following line
-          event.preventDefault();                               // this prevents the page from reloading
+      $('#new-blog-input').on('submit', function(event) {
+          event.preventDefault();
 
       // grab values of each input
           var newTitle = $('#title-input').val();
@@ -15,14 +14,16 @@ $(document).ready(function() {
           var newContent =$('#content-input').val();
           var newImage =$('#url-input').val();
 
+                                                          console.log(newImage);
+
       // change text of each blog part
           template.find('.title').text(newTitle);
           template.find('.author').text(newAuthor);
           template.find('.content').text(newContent);
           template.find('img').attr('src', newImage);
 
+                                                          console.log(  $('.title').text()  );
       // put the modified post into the DOM
           $('#blog-template').before(template);
       })
-
 })
